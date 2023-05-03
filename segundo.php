f7eaabe3e8632e2527df12e5ead616ecec505dd8
PissOff200 <andreearoxana534@gmail.com>
2023-05-03 16:37:33 +0200

    should work

--- segundo.php
+++ segundo.php
@@ -1,17 +1,14 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Primer PHP</title>
+    <title>Segundo PHP</title>
 </head>
 <body>
-    <h1>Página PHP</h1>
     <?php
-        //Código de PHP
-        echo "Hola mundo";
-        echo "PHP funcionando en LOCALHOST";
+    //Código PHP
+    echo "Hola segundo";
     ?>
 </body>
 </html>
