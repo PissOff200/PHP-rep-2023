f59ffa6a344af35457c755c3816c622489219d2c
PissOff200 <andreearoxana534@gmail.com>
2023-05-04 16:47:28 +0200

    Exercises and updates

---
+++ Exercise echo.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<h2> ¡PHP es divertido! </h2>";
+
+echo "¡Hola mundo! <br>";
+
+echo "¡Estoy a punto de aprender PHP! <br>";
+
+echo "Esta", " cadena de texto ", "se ha", "
+creado ", " con multiples parametros.";
+
+echo '<br> <a href="Exercise print.php"> ejercicio de print()</a>';
+?>
---
+++ Exercise print.php
@@ -0,0 +1,11 @@
+<?php
+$txt1 = "Aprende PHP";
+$txt2 = "pr0j3ct.com";
+$x = 5; $y = 4;
+print "<h2>" . $txt1 . "</h2>";
+print "Estudia PHP con " . $txt2 . "<br>";
+$variable1 = print $x + $y; //Le asigna un 1 a $variable1 porque tiene un return de un 1
+// $variable2 = echo ($x + $y); //No se le puede asignar a ninguna variable porque no devuelve nada
+print "<br>" . $variable1; //Mostrará un 1
+?>
+print '<br> <a href="Exercise echo.php"> ejercicio de echo ()</a>';
