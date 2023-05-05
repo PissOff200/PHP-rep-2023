574a220de43b297c17b4bc8a7d6b072493ac14e8
PissOff200 <andreearoxana534@gmail.com>
2023-05-05 16:18:16 +0200

    arrays type

---
+++ 7-Tipos/7.class-object-herencia-visibilidad.php
@@ -0,0 +1,80 @@
+<?php
+
+//HERENCIA
+class Foo
+{
+    public function printItem($string)
+    {
+        echo 'Foo: ' . $string . PHP_EOL;
+    }
+
+    public function printPHP()
+    {
+        echo 'PHP is great.' . PHP_EOL;
+    }
+}
+
+class bar extends Foo
+{
+    public function printItem($string)
+    {
+        echo 'Bar: ' . $string . PHP_EOL;
+    }
+}
+
+$foo = new Foo();
+$bar = new Bar();
+$foo->printItem('baz'); // Salida: 'Foo: baz'
+$foo->printPHP();       // Salida: 'PHP is great' 
+$bar->printItem('baz'); // Salida: 'Bar: baz'
+$bar->printPHP();       // Salida: 'PHP is great'
+
+//VISIBILIDAD
+/**
+ * Definicion de MyClass
+ */
+class MyClass
+{
+    public $public = 'Public';
+    protected $protected = 'Protected';
+    private $private = 'Private';
+
+    function printHello()
+    {
+        echo $this->public;
+        echo $this->protected;
+        echo $this->private;
+    }
+}
+
+$obj = new MyClass();
+echo $obj->public;    // Funciona bien
+echo $obj->protected; // Error Fatal
+echo $obj->private;   // Error Fatal
+$obj->printHello();   // Muestra Public, Protected y Private
+
+
+/**
+ * Definicion de MyClass2
+ */
+class MyClass2 extends MyClass
+{
+    // Se pueden redeclarar las propiedades publica y protegida, pero no la privada
+    public $public = 'Public2';
+    protected $protected = 'Protected2';
+
+    function printHello()
+    {
+        echo $this->public;
+        echo $this->protected;
+        echo $this->private;
+    }
+}
+
+$obj2 = new MyClass2();
+echo $obj2->public;    // Funciona bien
+echo $obj2->protected; // Error Fatal
+echo $obj2->private;   // Undefined
+$obj2->printHello();   // Muestra Public2, Protected2, Undefined
+
+?>
