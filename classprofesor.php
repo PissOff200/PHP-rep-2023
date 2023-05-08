6ef7b8ae4741d383207d1894619c312a3662ffa1
PissOff200 <andreearoxana534@gmail.com>
2023-05-08 16:27:54 +0200

    Update classprofesor

--- classprofesor.php
+++ classprofesor.php
@@ -2,50 +2,36 @@
 //Creamos la clase Profesor
     class Profesor {
         //PROPIEDADES del objeto
-
-        //Atributos
-        public $nombre = 'Marc';
-        public $apellido = 'Esteve';
-        public $altura = 1.75;
-        public $materia = "Desarrollo web";
-        //Modificadores de atributos/métodos "public/protected/private"
-        //echo $nombre; //no funciona
-
-        //Métodos
-        public function hablar($mensaje){
-            echo $mensaje;
-        }
+        //ATRIBUTOS
+        private $nombre;
+        private $materia;
+        //MÉTODOS
         //Getters y Setters
         public function getNombre() {
             return $this->nombre;
         }
-    
         public function setNombre($nombre) {
             $this->nombre = $nombre;
         }
-    
         public function getMateria() {
             return $this->materia;
         }
-    
         public function setMateria($materia) {
             $this->materia = $materia;
         }
     }
     
-    $profesor1 = new Profesor();//Creamos OBJETO
-    //echo gettype($profesor1); //Muestra tipo "object"
-    echo 'El nombre del profesor es ' . $profesor1->nombre . ' ' . $profesor1->apellido . " con una altura de " . $profesor1->altura . 'm' . " que imparte la asignatura de " . $profesor1->materia . "<br>";
-    $profesor1->hablar("Un cordial saludo");
-
-    //EJERCICIO: Muestra también en el texto: "imparte Desarrollo Web"
-
-    //Creamos un segundo OBJETO de la CLASS "Profesor"
-    $profesor2 = new Profesor(); //Instancia
-    $profesor2->nombre = "Juan"; // Asignaciones
-    $profesor2->materia = "Inglés";
-    echo "<br>";
-    echo 'El nombre del profesor es ' . $profesor2->nombre . " e imparte la asingatura " . $profesor2->materia;
-    $profesor2->hablar("<br>Buenas tardes");
+    $p1 = new Profesor();
+    //echo $p1->nombre; //No se puede porque los atributos son privados
+    $p1->setNombre("Juan");
+    echo $p1->getNombre(); // Imprime "Juan"
+    $p2 = new Profesor();
+    //echo $p2->nombre; //No se puede porque los atributos son privados
+    $p2->setNombre("Pedro");
+    echo $p2->getNombre(); // Imprime "Pedro"
+    $p3 = new Profesor();
+    //echo $p3->nombre; //No se puede porque los atributos son privados
+    $p3->setNombre("Manolo");
+    echo $p3->getNombre(); // Imprime "Manolo"
 
 ?>
